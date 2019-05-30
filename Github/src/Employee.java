2d4cb3c69448a76cf4e9ddb35a580f17b0a67a8a
8310576975 <bharat3311048@gmail.com>
2019-05-30 16:44:07 +0530

    Employee model

---
+++ Github/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+private int id;
+private String name;
+private Double sal;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Double getSal() {
+	return sal;
+}
+public void setSal(Double sal) {
+	this.sal = sal;
+}
+}
